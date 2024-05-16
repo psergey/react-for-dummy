04c51883359a92b8597f616b84ce266c1eb788a3
Sergiy Parkhomenko <sergey.parkhomenko@hotmail.com>
2024-05-16 18:11:38 +0200

    cleanup

--- src/common/utils/className.ts
+++
@@ -1,7 +0,0 @@
-const className = (defalt: string, classes: Record<string, boolean>): string =>
-  [
-    defalt ?? "",
-    ...Object.keys(classes).map((key) => (classes[key] ? key : "")),
-  ].join(" ");
-
-export default className;
